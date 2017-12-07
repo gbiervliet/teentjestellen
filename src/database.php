ef8d8d7794d038982191c853b3b19dbe8e5d3119
Gino Biervliet <gino@documentready.nl>
2017-12-07 16:52:29 +0100

    working flow

--- src/database.php
+++ src/database.php
@@ -1,7 +1,4 @@
 <?php
-	echo 'Rico is gek';
-	
-
 	// function getDatabaseData() {
 	// 	include 'credentials.php';
 	// 	try {
@@ -20,12 +17,9 @@
 
 	function putChoiceInDB($choice) {
 		include 'credentials.php';
-		echo 'set choice '.$choice.'<br>';
-
 		try {
 		    $dbh = new PDO('mysql:host=' . $serverAddr .';dbname=teentjestellen_nl_teentjesdb', $username, $password);
 			   if($choice == "boy") {
-			   	echo 'chosen a boy<br>';
 			   	$dbh->query('UPDATE gender SET votes=votes+1 WHERE id=0');
 			   } else {
 			    $dbh->query('UPDATE gender SET votes=votes+1 WHERE id=1');
--- src/gender.php
+++ src/gender.php
@@ -2,16 +2,10 @@
 	include 'database.php';
 
 	$choice = $_POST["gender"];
-	echo 'chosen<br>';
 	if($choice == "female") {
-		echo 'chosen a girl<br>';
 		putChoiceInDB('girl');
 
 	} else {
-		echo 'chosen a boy<br>';
 		putChoiceInDB('boy');
 	}
-
-	// getDatabaseData();
-
 ?>
